c5157ef93a6b31b91d98e4340614687924ce5693
Kane Wright <kanewright@live.com>
2020-05-12 20:06:42 +0300

    Initial code

---
+++ PercentCalculator/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace PercentCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBoxRoundTo = new System.Windows.Forms.ComboBox();
+            this.labelRoundTo = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(36, 12);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(253, 20);
+            this.textBoxA.TabIndex = 0;
+            this.textBoxA.Text = "1";
+            this.textBoxA.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            this.textBoxA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxA_KeyPress);
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(36, 39);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(253, 20);
+            this.textBoxB.TabIndex = 1;
+            this.textBoxB.Text = "1";
+            this.textBoxB.TextChanged += new System.EventHandler(this.textBoxB_TextChanged);
+            this.textBoxB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxB_KeyPress);
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(13, 15);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(17, 13);
+            this.labelA.TabIndex = 2;
+            this.labelA.Text = "A:";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(13, 42);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(17, 13);
+            this.labelB.TabIndex = 3;
+            this.labelB.Text = "B:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 92);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(273, 131);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(267, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(267, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(3, 99);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(267, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // comboBoxRoundTo
+            // 
+            this.comboBoxRoundTo.Location = new System.Drawing.Point(168, 65);
+            this.comboBoxRoundTo.Name = "comboBoxRoundTo";
+            this.comboBoxRoundTo.Size = new System.Drawing.Size(36, 21);
+            this.comboBoxRoundTo.TabIndex = 5;
+            this.comboBoxRoundTo.SelectedIndexChanged += new System.EventHandler(this.comboBoxRoundTo_SelectedIndexChanged);
+            // 
+            // labelRoundTo
+            // 
+            this.labelRoundTo.AutoSize = true;
+            this.labelRoundTo.Location = new System.Drawing.Point(16, 68);
+            this.labelRoundTo.Name = "labelRoundTo";
+            this.labelRoundTo.Size = new System.Drawing.Size(136, 13);
+            this.labelRoundTo.TabIndex = 6;
+            this.labelRoundTo.Text = "Разрядов после запятой:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 233);
+            this.Controls.Add(this.labelRoundTo);
+            this.Controls.Add(this.comboBoxRoundTo);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PercentCalculator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBoxRoundTo;
+        private System.Windows.Forms.Label labelRoundTo;
+    }
+}
+
---
+++ PercentCalculator/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PercentCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = "";
+            textBox1.ReadOnly = true;
+            textBox1.BorderStyle = 0;
+            textBox1.BackColor = this.BackColor;
+            textBox1.TabStop = false;
+            textBox2.Text = "";
+            textBox2.ReadOnly = true;
+            textBox2.BorderStyle = 0;
+            textBox2.BackColor = this.BackColor;
+            textBox2.TabStop = false;
+            textBox3.Text = "";
+            textBox3.ReadOnly = true;
+            textBox3.BorderStyle = 0;
+            textBox3.BackColor = this.BackColor;
+            textBox3.TabStop = false;
+            textBox4.Text = "";
+            textBox4.ReadOnly = true;
+            textBox4.BorderStyle = 0;
+            textBox4.BackColor = this.BackColor;
+            textBox4.TabStop = false;
+            textBoxA.MaxLength = 29;    //decimal.MaxValue.ToString().Length;
+            textBoxB.MaxLength = 29;    //decimal.MaxValue.ToString().Length;
+            comboBoxRoundTo.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxRoundTo.Items.Add(0);
+            comboBoxRoundTo.Items.Add(1);
+            comboBoxRoundTo.Items.Add(2);
+            comboBoxRoundTo.Items.Add(3);
+            comboBoxRoundTo.SelectedIndex = 2;
+            UpdateForm();
+        }
+
+        private void UpdateForm()
+        {
+            decimal.TryParse(textBoxA.Text, out decimal aInput);
+            decimal.TryParse(textBoxB.Text, out decimal bInput);
+            decimal a = 0, b = 0;
+            try
+            {
+                //if (string.IsNullOrEmpty(textBoxA.Text) || a <= 0 || b <= 0)
+                //    throw new Exception("Invalid input");
+
+                if (aInput > bInput || aInput == bInput)
+                {
+                    a = aInput;
+                    b = bInput;
+
+                }
+                //else if (aInput < bInput)
+                else
+                {
+                    b = aInput;
+                    a = bInput;
+                }
+                textBox1.Text = string.Format($"{a} составляет {WhatPercentOf(a, b, (int)comboBoxRoundTo.SelectedItem):0.###}% от {b}");
+                textBox2.Text = string.Format($"{b} составляет {WhatPercentOf(b, a, (int)comboBoxRoundTo.SelectedItem):0.###}% от {a}");
+                textBox3.Text = string.Format($"{a} больше чем {b} на {PercentageChange(a, b, (int)comboBoxRoundTo.SelectedItem):0.###}%");
+                textBox4.Text = string.Format($"{b} меньше чем {a} на {PercentageChange(b, a, (int)comboBoxRoundTo.SelectedItem):0.###}%");
+            }
+            catch (Exception)
+            {
+                textBox1.Text = "Invalid input";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+            }
+        }
+        private decimal WhatPercentOf(decimal a, decimal b, int n)
+        {
+            return Math.Round(a / b * 100, n);
+        }
+
+        private decimal PercentageChange(decimal a, decimal b, int n)
+        {
+            if (a > b)
+                return Math.Round((a - b) / b * 100, n);
+            else if (a < b)
+                return Math.Round((b - a) / b * 100, n);
+            else
+                return 0;
+        }
+        private void textBoxA_TextChanged(object sender, EventArgs e)
+        {
+            UpdateForm();
+        }
+
+        private void textBoxB_TextChanged(object sender, EventArgs e)
+        {
+            UpdateForm();
+        }
+
+        private void textBoxA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != Convert.ToChar(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != Convert.ToChar(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void comboBoxRoundTo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateForm();
+        }
+    }
+}
